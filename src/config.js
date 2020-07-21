d2c428b42cb329a5afaf10558dbc0fdbc23c256d
unknown <exgin3538@yahoo.com>
2020-07-21 15:24:39 -0400

    ignore config file

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
-const webpack = require('webpack');
-const dotenv = require('dotenv');
+// const webpack = require('webpack');
+// const dotenv = require('dotenv');
 
-module.exports = () => {
-  const env = dotenv.config().parsed; // returns an object with all of the env vars in .env
+// module.exports = () => {
+//   const env = dotenv.config().parsed; // returns an object with all of the env vars in .env
 
-  const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-  }, {});
+//   const envKeys = Object.keys(env).reduce((prev, next) => {
+//     prev[`process.env.${next}`] = JSON.stringify(env[next]);
+//     return prev;
+//   }, {});
 
-  console.log(envKeys);
+//   console.log(envKeys);
 
-  return {
-    plugins: [new webpack.DefinePlugin(envKeys)],
-  };
-};
+//   return {
+//     plugins: [new webpack.DefinePlugin(envKeys)],
+//   };
+// };
