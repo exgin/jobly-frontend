223d8cb4736040ddde0a1ac7b2564dd5adae8cd6
unknown <exgin3538@yahoo.com>
2020-07-21 16:38:03 -0400

    export

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://localhost/3000';
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://localhost/3000';
+
+export default BACKEND_URL;
