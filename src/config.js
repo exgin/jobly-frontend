521152bc7cf44dd255dbaf0e535381795738897f
unknown <exgin3538@yahoo.com>
2020-07-21 14:57:42 -0400

    changing config file

--- src/config.js
+++ src/config.js
@@ -1,3 +1,15 @@
-require('dotenv').config();
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 
-export const URL = process.env.URL_LINK || 'http://localhost:3001';
+module.exports = () => {
+  const env = dotenv.config().parsed; // returns an object with all of the env vars in .env
+
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    plugins: [new webpack.DefinePlugin(envKeys)],
+  };
+};
